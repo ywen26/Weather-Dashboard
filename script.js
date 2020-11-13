685cb2a7de5276c22e2275e51b0c3919096d38b0
Wednesday <yensonyu@gmail.com>
2020-11-12 21:20:41 -0800

    Update city list function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var cities = [];
+// var cities = [];
+var citiesEl = $("#cities-view");
 
 // function renderCities() {
 //     $("#cities-view").empty();
@@ -11,9 +12,48 @@ var cities = [];
 $("#add-city").on("click", function(event) {
     event.preventDefault();
     var inputEl = $("#city-input").val();
-    var citiesEl = $("<p>").text(inputEl);
-    $("#cities-view").prepend(citiesEl);
+    var cityName = {city: inputEl};
+    var cityList = localStorage.getItem("cityList");
+
+    if (cityList === null) {
+        cityList = [];
+    } 
+    else {
+        cityList = JSON.parse(cityList);
+    }
+
+    cityList.push(cityName);
+
+    var newAddCity = JSON.stringify(cityList);
+    localStorage.setItem("cityList", newAddCity);
+    // var inputEl = $("#city-input").val();
+    // var citiesEl = $("<p>").text(inputEl);
+    // $("#cities-view").prepend(citiesEl);
     // renderCities();
-})
+    location.reload();
+});
 
 // renderCities();
+
+cityList = localStorage.getItem("cityList");
+cityList = JSON.parse(cityList);
+
+if (cityList !== null) {
+    for (var i = 0; i < cityList.length; i++) {
+        var cities = $("<p>");
+        cities.text(cityList[i].city);
+        citiesEl.prepend(cities);
+    }
+}
+
+var clear = $("<button>");
+$("#info").append(clear);
+clear.on("click", function() {
+    localStorage.clear();
+    location.reload();
+})
+
+
+
+
+
